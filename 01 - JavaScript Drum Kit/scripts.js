b82f710b4cca1439ed3933ebe4b86589f976c35a
Jon Maldia <jon@maldia.net>
2018-03-09 22:32:11 -0500

    Add JS to get drum sound function

---
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -0,0 +1,30 @@
+function playSound(e) {
+	// selects element based on keys pressed and their values
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`.keys[data-key="${e.keyCode}"]`);
+	
+	// prevents keys with no audio associated with it from doing anything
+	if(!audio) return;
+	// rewinds audio so that it goes back to the beginning everytime you hit a key
+	audio.currentTime = 0;
+	// play audio on key press
+	audio.play();
+
+	// highlight and animate key
+	key.classList.add("pressed");
+}
+
+// funtion to remove the css that highlights and animates a key/
+function removeTransition(e) {
+	if(e.propertyName !== 'transform') return;
+	this.classList.remove("pressed");
+}
+
+const keys = document.querySelectorAll('.keys');
+// listen to each key on the keyboard that is matched that has a transitionend
+keys.forEach(function(key) {
+	key.addEventListener('transitionend', removeTransition);
+});
+
+// listens for keys pressed
+window.addEventListener('keydown', playSound);
